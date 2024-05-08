1877236c5969f5593d8ab6b710785f6e26f954d9
David Doll <doll.david0395@gmail.com>
2024-05-08 05:34:00 -0500

    Added missing ProjectController class file that was meant to be in the last push to the repository.

---
+++ ProjectManager/ProjectManager/Controllers/ProjectController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManager.Data;
+
+namespace ProjectManager.Controllers;
+
+[Route("api/project")]
+[ApiController]
+public class ProjectController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    
+    public ProjectController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var projects = _context.Project.ToList();
+
+        return Ok(projects);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetById(Guid id)
+    {
+        var project = _context.Project.Find(id);
+
+        if (project == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(project);
+    }
+}
